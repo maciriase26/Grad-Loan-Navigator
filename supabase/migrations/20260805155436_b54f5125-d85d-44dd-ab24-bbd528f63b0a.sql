8230401fac004a9193e5e7b2f7d33d8908d821f5
maciriase26 <amazingfaith011@gmail.com>
2026-08-10 15:59:46 -0500

    fixing supabase migration issue

--- supabase/migrations/20260805155436_b54f5125-d85d-44dd-ab24-bbd528f63b0a.sql
+++
@@ -1,6 +0,0 @@
-REVOKE ALL ON FUNCTION public.request_category_summary() FROM PUBLIC;
-REVOKE ALL ON FUNCTION public.request_category_summary() FROM anon;
-REVOKE ALL ON FUNCTION public.request_category_summary() FROM authenticated;
-REVOKE ALL ON FUNCTION public.set_updated_at() FROM PUBLIC;
-REVOKE ALL ON FUNCTION public.set_updated_at() FROM anon;
-REVOKE ALL ON FUNCTION public.set_updated_at() FROM authenticated;
